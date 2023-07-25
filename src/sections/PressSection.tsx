e36b541634ed0758ef9baf697ff38d5a227a82b3
Tyler Savery <tyler@theyoungastronauts.com>
2023-07-25 16:20:36 -0400

    update

--- src/sections/PressSection.tsx
+++ src/sections/PressSection.tsx
@@ -5,6 +5,14 @@ import styles from '../../styles/sections/PressSection.module.scss'
 
 const pressItems = [
     {
+        body: "Capturing the free-flowing energy and raw vitality of his live set, it’s an album filled with feverish guitar solos, sing-along-ready choruses, and soulful vocals, marking a vibrant new chapter for the California native.",
+        from: "Ticketmaster",
+    },
+    {
+        body: "The new King of Rock and Roll…this kid can sing, he can dance, and - holy s**t* - he can play. Hitting his notes with perfection like a euphoric guitar hero. Watching GUNNAR was like watching Elvis Presley reincarnated.",
+        from: "Daily Express",
+    },
+    {
         body: "The rebellious spirit is really the lifeblood of rock n’ roll music, always has been, and always will be. Young musician GUNNAR is carving out quite a path for himself, with his middle finger firmly pointed in the air",
         from: "V13"
     },
@@ -20,14 +28,7 @@ const pressItems = [
         body: "The rising start to have on your radar right now",
         from: "Wonderland Magazine"
     },
-    {
-        body: "Capturing the free-flowing energy and raw vitality of his live set, it’s an album filled with feverish guitar solos, sing-along-ready choruses, and soulful vocals, marking a vibrant new chapter for the California native.",
-        from: "Ticketmaster",
-    },
-    {
-        body: "The new King of Rock and Roll…this kid can sing, he can dance, and - holy s**t* - he can play. Hitting his notes with perfection like a euphoric guitar hero. Watching GUNNAR was like watching Elvis Presley reincarnated.",
-        from: "Daily Express",
-    },
+
 
 ];
 
